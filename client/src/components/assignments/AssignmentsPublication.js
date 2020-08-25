15f4ae36acdaa55c1e7ca2dd36efb09292bcf965
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-08-25 14:36:22 -0300

    [master] - first version of last assignment step

--- client/src/components/assignments/AssignmentsPublication.js
+++ client/src/components/assignments/AssignmentsPublication.js
@@ -1,24 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import {
+    Paper,
+    Box,
+    Typography,
+    FormControlLabel,
+    Checkbox,
+    ListItemIcon,
+    ListItem,
+    List
+} from '@material-ui/core';
 
-export default function AssignmentsPreview() {
+import { useRootContext } from '../RootContext';
+import FabLoadingProgress from '../FabLoadingProgress';
+import PopupModal from '../PopupModal';
+
+export default function AssignmentsPublication() {
+    const { store, dispatch } = useRootContext().assignments;
+    const { contacts } = store;
+    const [success, setSuccess] = useState(false);
+    const [modalMessage, setModalMessage] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [checkboxes, setCheckbox] = useState({});
 
     const send = async () => {
-        const { data } = await axios.post(`${process.env.REACT_APP_ROOT}/api/v1/assignments`, {
-            to: [
-                { email: 'name@example.com', name: 'Name Example' },
-            ]
-        });
+        try {
+            setLoading(true);
+            const emailsList = Object.keys(checkboxes).filter(k => checkboxes[k]).map(t => {
+                const foundContact = contacts.find(c => c.value === t);
+                return {
+                    email: foundContact.value,
+                    name: foundContact.text,
+                }
+            });
+            const { data } = await axios.post(`${process.env.REACT_APP_ROOT}/api/v1/assignments`, { to: emailsList });
 
-        if (data) {
-            alert('teste foi enviado!');
+            if (data) {
+                setSuccess(true);
+                setLoading(false);
+                dispatch({ type: 'SET_EMAIL_SENT', data: true });
+                setModalMessage(<>
+                    <Typography variant="button" color="primary">Email enviado para:</Typography>
+                    {Object.keys(checkboxes).filter(k => checkboxes[k]).map(contact => (
+                        <Typography variant="body1" key={contact}>
+                            {contact}
+                        </Typography>
+                    ))}
+                </>);
+            }
+        } catch (error) {
+            setSuccess(false);
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <div>
-            <p>CLIQUE PARA ENVIAR</p>
-            <button onClick={send}>ENVIAR EMAIL DE TESTE</button>
-        </div>
+        <Paper elevation={2}>
+            <PopupModal
+                isOpen={Boolean(modalMessage)}
+                content={modalMessage}
+                confirmOnly={true}
+                onClose={() => setModalMessage(false)}
+                onConfirm={() => setModalMessage(false)}
+            />
+
+            <Box m={1} mt={3} p={1} pb={5} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+                <Typography variant="subtitle1">Selecione os contatos que deseja incluir no email:</Typography>
+                <List>
+                    {contacts && contacts.map(({ text: name, value: address }, index) => (
+                        <ListItem key={`${address}-${index}`} dense>
+                            <ListItemIcon>
+                                <FormControlLabel
+                                    control={<Checkbox color="primary" name={address} />}
+                                    label={`${name} (${address})`}
+                                    onChange={e => setCheckbox({ ...checkboxes, [address]: e.target.checked })}
+                                />
+                            </ListItemIcon>
+                        </ListItem>
+                    ))}
+                </List>
+                <FabLoadingProgress success={success} loading={loading} onClick={send} />
+            </Box>
+        </Paper >
     );
 }
