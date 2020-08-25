3bfec27224806ee9dc69a8854fcd0f2861cef956
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-08-25 14:32:45 -0300

    [master] - exposing StepByStep navigation controls and handling view states by root context

--- client/src/components/StepByStep.js
+++ client/src/components/StepByStep.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Check, ChevronRight } from '@material-ui/icons';
 import {
@@ -13,13 +13,38 @@ import {
     Typography
 } from '@material-ui/core';
 
-import FabLoadingProgress from './FabLoadingProgress';
+import { useRootContext } from './RootContext';
 
-export default function StepByStep({ steps = [], feedback, conclusionStep }) {
+export default function StepByStep({ steps = [], feedback, conclusionStep, onConclude, onBack, onNext, onReset }) {
+    const { store, dispatch } = useRootContext().assignments;
     const [activeStep, setActiveStep] = useState(0);
-    const [success, setSuccess] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const timer = useRef();
+
+    const isFirst = activeStep === 0;
+    const hasNext = activeStep < steps.length - 1;
+    const hasCompleted = activeStep === steps.length;
+
+    const getStepHint = () => steps[activeStep] ? steps[activeStep].hint : '';
+
+    const handleBack = () => {
+        onBack && onBack(activeStep - 1);
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    }
+    const handleNext = async () => {
+        if (!steps[activeStep].callback || await steps[activeStep].callback()) {
+            if (hasCompleted) {
+                onConclude && onConclude();
+            } else {
+                onNext && onNext(activeStep + 1);
+            }
+            setActiveStep(prevActiveStep => prevActiveStep + 1);
+        }
+    }
+    const handleReset = () => {
+        dispatch({ type: 'SET_EMAIL_SENT', data: false });
+        onReset();
+        setActiveStep(0);
+    };
+
     const theme = useTheme();
     const classes = makeStyles(() => ({
         instructions: {
@@ -34,38 +59,6 @@ export default function StepByStep({ steps = [], feedback, conclusionStep }) {
             marginRight: theme.spacing(1)
         }
     }))();
-
-    const isFirst = activeStep === 0;
-    const isLast = activeStep === steps.length - 1;
-    const hasNext = activeStep < steps.length - 1;
-    const hasCompleted = activeStep === steps.length;
-
-    const getStepHint = () => steps[activeStep] ? steps[activeStep].hint : '';
-
-    const handleBack = () => setActiveStep(prevActiveStep => prevActiveStep - 1);
-    const handleNext = async () => {
-        if (!steps[activeStep].callback || await steps[activeStep].callback()) {
-            setActiveStep(prevActiveStep => prevActiveStep + 1);
-        }
-    }
-    const handleReset = () => {
-        setSuccess(false);
-        setLoading(false);
-        setActiveStep(0);
-    };
-    const handleButtonClick = () => {
-        setSuccess(false);
-        setLoading(true);
-        timer.current = setTimeout(() => {
-            setSuccess(true);
-            setLoading(false);
-            handleNext();
-        }, 3000);
-    };
-
-    useEffect(() => {
-        return () => clearTimeout(timer.current);
-    }, []);
 
     return (
         <div>
@@ -102,18 +95,14 @@ export default function StepByStep({ steps = [], feedback, conclusionStep }) {
                         </Typography>
                     </Hidden>
                     <Box display="flex" flexDirection="row" height={theme.spacing(5)} mr="auto" ml="auto" justifyContent="center">
-                        {!hasCompleted && <Button className={classes.spacing} disabled={isFirst} color="primary" onClick={handleBack}>Anterior</Button>}
+                        {!hasCompleted && !store.emailSent && <Button className={classes.spacing} disabled={isFirst} color="primary" onClick={handleBack}>Anterior</Button>}
                         {hasNext && <Button className={classes.spacing} variant="contained" color="primary" onClick={handleNext}>Próximo</Button>}
-                        {hasCompleted && <Button variant="contained" color="secondary" onClick={handleReset}>Preciso refazer</Button>}
-                        {(isLast || hasCompleted) && <FabLoadingProgress success={success} loading={loading} onClick={handleButtonClick} />}
+                        {store.emailSent && <Button variant="contained" color="secondary" onClick={handleReset}>Preciso refazer</Button>}
                     </Box>
                 </Box>
             </Paper>
 
-            {hasCompleted
-                ? conclusionStep
-                : (steps[activeStep] || {}).content
-            }
+            {hasCompleted ? conclusionStep : (steps[activeStep] || {}).content}
         </div>
     );
 }
